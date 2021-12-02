3c4b01b8de42ec7a65cb2eb05d187b5c74c3fad1
zl248 <zl248@duke.edu>
2021-12-01 23:05:18 -0500

    fix: babelrc corejs

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -10,7 +10,7 @@ function Homepage() {
                 123 <div id="logo">456</div>{' '}
             </div>
             <p>App demo</p>
-            { status === null ? < Guidance/> : null}
+            {/* { status === null ? < Guidance/> : null} */}
             {/* { status === "signin" ? <Signin/> : null}
             { status === "register" ? <Register/>: null} */}
         </>
