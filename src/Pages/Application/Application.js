29c169278c244c100878781b623dad6557a2111d
zl248 <zl248@duke.edu>
2021-12-04 10:19:06 -0500

    feat: router config

--- src/Pages/Application/Application.js
+++ src/Pages/Application/Application.js
@@ -1,7 +1,41 @@
 import React, { useState } from 'react';
-
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+// import { browserHistory } from 'react-router';
+import routes from '../../router';
 function Application() {
-  
+    return (
+        <BrowserRouter>
+            <div style={{ display: 'flex' }}>
+                <div
+                    style={{
+                        width: '25%',
+                    }}
+                >
+                    <ul style={{ listStyleType: 'none', padding: 0 }}>
+                        <li>
+                            <Link to="/">My schedule</Link>
+                        </li>
+                        <li>
+                            <Link to="history">History</Link>
+                        </li>
+                        <li>
+                            <Link to="events">Events</Link>
+                        </li>
+                        <li>
+                            <Link to="contact">Contacts</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div id="mainApp" style={{ flex: 1 }}>
+                    <Routes>
+                        {routes.map((route, index) => (
+                            <Route key={index} path={route.path} element={route.main} />
+                        ))}
+                    </Routes>
+                </div>
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default Application;
