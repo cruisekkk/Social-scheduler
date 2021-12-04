d1df636c7aeeb165309db4f596518bdb3a462ce0
zl248 <zl248@duke.edu>
2021-12-04 00:16:29 -0500

    frame: app & homepage division

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { useState } from 'react';
 import Homepage from './Pages/Homepage/Homepage.js';
+import Applicaiton from './Pages/Application/Application.js';
 import 'antd/dist/antd.css'
 function App() {
-    // const [count, setCount] = useState(0);
-    return (
+    const [isLogedIn, setIsLogedIn] = useState(false);
+
+    const renderPage = () => {
+      if (isLogedIn) {
+        return (
         <div id="app">
           <Homepage/>
+        </div>)
+      } else {
+        return (
+          <div id="app">
+          <Application/>
         </div>
-    );
+        )
+      }
+    }
+    return renderPage();
 }
+
 
 export default App;
---
+++ src/Pages/Application/Application.js
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+function Application() {
+  
+}
+
+export default Application;
