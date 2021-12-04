a235a96947eac87f2d4a68fe32532cdbfe56b236
alice <zhenshengxie@Zhenshengs-Air.lan>
2021-12-04 18:56:23 -0500

    complete calendar and share button

---
+++ src/Component/AppCalendar/AppCalendar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./AppCalendar.less";
+
+function AppCalendar() {
+    return (
+        <div id="box">
+            <img id="calendar"
+                    src="http://localhost:8000/calendar.png"
+            />  
+        </div>
+    );
+}
+
+export default AppCalendar;
