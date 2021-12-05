795c24631b99e72f2d7129d2d5f1763581e9b7a1
alice <zhenshengxie@Zhenshengs-Air.lan>
2021-12-04 20:47:47 -0500

    complete share popup box

--- src/Component/ShareBox/ShareBox.js
+++ src/Component/ShareBox/ShareBox.js
@@ -1,16 +1,56 @@
-import React from 'react';
-import { Button } from 'antd';
+import React, { useState } from 'react';
+import { Button, Modal, message, Input } from 'antd';
 
 function ShareBox() {
+    const [isShareVisible, setIsShareVisible] = useState(false);
+
+    const showModal = () => {
+        setIsShareVisible(true);
+    };
+
+    const handleOk = () => {
+        message.success('successfully shared');
+        setIsShareVisible(false);
+    };
+
+    const handleCancel = () => {
+        setIsShareVisible(false);
+    };
+
     return (
-        <Button 
-            size="large" 
-            style={{
-                backgroundColor : "#F39946",
-                margin: "50% 43%"
-        }}>
-            Share
-        </Button>
+        <>
+            <Button 
+                size="large" 
+                style={{
+                    backgroundColor : "#F39946",
+                    margin: "50% 43%"
+                }}
+                onClick={showModal}
+            >
+                Share
+            </Button>
+            <Modal 
+                title="Send link" 
+                visible={isShareVisible} 
+                onOk={handleOk} 
+                onCancel={handleCancel} 
+                footer={
+                        [] 
+            }>
+                <div>
+                    <div style={{position: "relative" }}>
+                        <Input.Group compact id="inputPlace">
+                            <Input style={{ width: 'calc(100% - 200px)' }} defaultValue="yichen.qian@duke.edu"/>
+                            <Button style={{backgroundColor: "#EE6C4D"}} onClick={handleOk}>Send invite</Button>
+                        </Input.Group>
+                    </div>
+                    <img 
+                        src="http://localhost:8000/shareBoxButtom.png"
+                        style={{width: '472px'}}
+                    />
+                </div>
+            </Modal>
+        </>
     );
 }
 
